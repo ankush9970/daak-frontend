66b5725f75ee421c07a714d7804b76803e5f5f44
Ankush9970 <64217970+ankush9970@users.noreply.github.com>
2025-08-13 18:44:06 +0000

    manageGroup_all_done

--- src/ManageGroup.jsx
+++ src/ManageGroup.jsx
@@ -3,6 +3,7 @@ import api from "./api";
 import { useAuth } from "./AuthContext";
 import toast from "react-hot-toast";
 import { hasPermission } from "./utils/Permission";
+import { FaSpinner } from "react-icons/fa";
 
 const ManageGroup = () => {
   const { user } = useAuth();
@@ -11,8 +12,11 @@ const ManageGroup = () => {
   const [showModal, setShowModal] = useState(false);
   const [fullName, setFullName] = useState("");
   const [shortName, setShortName] = useState("");
+  const [groupId, setGroupId] = useState(null);
 
   const fetchGroups = async () => {
+    setLoading(true);
+
     try {
       const res = await api.get("/users/group");
       localStorage.getItem("role") !== "admin"
@@ -21,23 +25,48 @@ const ManageGroup = () => {
       // setGroups(res.data);
     } catch (err) {
       console.error("Failed to fetch groups:", err);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const editGroup = async (userId) => {
+  const editGroup = async (e) => {
+    e.preventDefault();
+    if (!fullName) return toast.error("Name is required");
+    if (!shortName) return toast.error("ShortName is required");
     setLoading(true);
+
     try {
-      const res = await api.put(`/users/group`, {
-        userId,
-        fullName,
-        shortName,
-      });
-      toast.success(res.data.message);
+      if (groupId) {
+        const res = await api.put(`/users/group`, {
+          groupId,
+          name: fullName,
+          shortName,
+        });
+        toast.success(res.data.message);
+        setLoading(false);
+        setShowModal(false);
+        setGroupId(null);
+        setFullName("");
+        setShortName("");
+      } else {
+        const res = await api.post(`/users/group`, {
+          name: fullName,
+          shortName,
+        });
+        toast.success(res.data.message);
+        setLoading(false);
+        setShowModal(false);
+        setGroupId(null);
+        setFullName("");
+        setShortName("");
+      }
     } catch (error) {
       console.log(error);
-      toast.error("failed to reset password");
+      toast.error(error.response.data.message);
     } finally {
       setLoading(false);
+      fetchGroups();
     }
   };
 
@@ -57,13 +86,17 @@ const ManageGroup = () => {
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">Manage Group</h1>
         <button
-          onClick={() => setShowModal(true)}
+          onClick={() => {
+            setShowModal(true);
+            setGroupId(null);
+            setFullName("");
+            setShortName("");
+          }}
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
         >
           + Add Group
         </button>
       </div>
-      {/* <h2 className="text-2xl font-bold mb-4">Manage Users & Roles</h2> */}
       <div className="overflow-x-auto border rounded shadow bg-white">
         <table className="min-w-full text-sm">
           <thead className="bg-gray-100">
@@ -92,7 +125,12 @@ const ManageGroup = () => {
                     {hasPermission("MANAGE_GROUP") && (
                       <td className="px-4 py-2 border capitalize">
                         <button
-                          onClick={(e) => editGroup(u._id)}
+                          onClick={() => {
+                            setGroupId(u._id);
+                            setFullName(u.name);
+                            setShortName(u.shortName);
+                            setShowModal(true);
+                          }}
                           className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
                         >
                           Edit
@@ -109,7 +147,9 @@ const ManageGroup = () => {
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/50">
           <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
-            <h2 className="text-xl font-bold mb-4">Edit Group</h2>
+            <h2 className="text-xl font-bold mb-4">
+              {groupId ? "Edit" : "Add"} Group
+            </h2>
             <form onSubmit={editGroup} className="space-y-4">
               <div>
                 <label className="block mb-1 text-sm font-medium">Name</label>
@@ -135,16 +175,23 @@ const ManageGroup = () => {
               <div className="flex justify-end space-x-2">
                 <button
                   type="button"
-                  onClick={editGroup}
+                  onClick={(e) => {
+                    setShowModal(false);
+                    setGroupId(null);
+                  }}
                   className="px-4 py-2 rounded bg-gray-300"
                 >
                   Cancel
                 </button>
                 <button
                   type="submit"
+                  disabled={loading}
                   className="px-4 py-2 rounded bg-blue-600 text-white"
                 >
-                  Update Password
+                  {loading ? (
+                    <FaSpinner className="animate-spin inline mr-2" />
+                  ) : null}
+                  {groupId ? "Update" : "Add"}
                 </button>
               </div>
             </form>
